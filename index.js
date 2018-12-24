cce285dac5fbba069f22ba0a2b3ca5e044b510a9
Ranveer Dhaliwal <mythlegendx@gmail.com>
2018-12-23 21:06:19 -0800

    Fixing structure to properly deploy web app

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/api/games', (req, res) => {
   const PLACEHOLDER_DATA = {
@@ -65,7 +65,7 @@ app.get('/api/games', (req, res) => {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/../client/build/index.html'));
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
