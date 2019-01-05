d8abd8648a790446c1a8b9d38476ded2ab99709a
Ranveer Dhaliwal <mythlegendx@gmail.com>
2019-01-04 21:25:38 -0800

    Adding some more unit tests

--- client/src/__mocks__/axios.js
+++ client/src/__mocks__/axios.js
@@ -1,5 +1,6 @@
 const mockAxios = jest.genMockFromModule('axios');
 
+// https://stackoverflow.com/questions/51393952/mock-inner-axios-create/51414152#51414152
 // this is the key to fix the axios.create() undefined error!
 mockAxios.create = jest.fn(() => mockAxios);
 
